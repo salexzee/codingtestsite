b085b94c833e2b6b395fd2cb8397cbcbd4d92dde
Sam Webb <samwebb@codingtestsite.com>
2014-07-28 10:28:01 +0930

    Content change on landing page

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 				<img class="mainLogo col-xs-5 col-sm-4 img-rounded" src="images/ctsLogo.png">
 				<p class="lead col-xs-12 col-sm-8">Hey, my name is Sam Webb and welcome to CodingTestSite. This is the place to be for up-to-date insight, tutorials and reviews on all things web design and web development. In the past few months I've done a lot of learning about quite a few different technologies and now I'd like to give back some of the information I've attained. Hope you learn a thing or two.</p>
 
-				<p class=" col-xs-12">If you have any questions or would like me to write a post about anything in particular, please feel free to <a href="contact.php">contact me</a>. My goal is to cover all the technologies that I've already worked with and then I'll move on to the things I'm not so comfortable with. I'm skilled with HTML, CSS Javascript, jQuery, PHP, Sass and a few others. I'm still working on learning MySQL and I'm also trying to expand my knowledge of Javascript and PHP. I have a lot to talk about so I'll leave it for the upcoming blog posts.</p>
 			</div>
 		</div> 
 	</main>
