3fa6dbce6227de277823cda44bbeb3e09595b40e
Sam Webb <samwebb@codingtestsite.com>
2014-08-22 15:07:57 +0930

    completed code snippets blog post

---
+++ blogs/code-snippets.php
@@ -0,0 +1,42 @@
+<?php include("../inc/blog-header.php"); ?>
+
+<section class="container col-xs-10 blog-posts">
+
+	<h1>Why Save Code Snippets?</h1>
+
+	<p>How many of you out there still manually type in your starting HTML? I'll admit it, I still do sometimes. I just haven't been able to completely break the habbit yet. A lot of the projects a beginner will be working on will probably be fairly small, so its not such a big deal, but what about when you want you're about to start a large project and you want to save all the time you can. That's when code snippets come in handy.</p>
+
+	<p>What are code snippets you may ask? Well I'll tell you. In simplest terms, they're pieces of code that you have saved somewhere so that you can use them in your projects. You don't need to make a code snippet for everything you do while coding, but what you want to create them for are the pieces of code you use most often. Take this for instance:</p>
+
+	<pre>
+&lt;!DOCTYPE html&gt;
+&lt;html lang="en"&gt;
+&lt;head&gt;
+	&lt;title&gt;Example Title&lt;/title&gt;
+	&lt;meta charset="utf-7"&gt;
+	&lt;link rel="stylesheet" href="css/example.css"&gt;
+&lt;/head&gt;
+&lt;body&gt;
+
+	&lt;header&gt;
+	&lt;/header&gt;
+
+	&lt;main&gt;
+	&lt;/main&gt;
+
+	&lt;footer&gt;
+	&lt;/footer&gt;
+
+&lt;/body&gt;
+&lt;/html&gt;
+	</pre>
+
+	<p>This is the kind of thing I use in almost every project I work on. It might not be much, but when trying to save time, this is something you could save and use later so that you don't have to type it in every time. Over time when you're working on a large project, if you have a bunch of little or big snippets that you can insert, you'll end up with more time to work on any issues and bugs in your code.</p>
+
+	<p>Depending on which text editor you use, you may be able to save your own code snippets so that you can call them with keywords. Sublime Text is one example of a text editor with that capability. Also, with ST, you can download code snippet packages people have made for your favorite languages and use those to save time. In the future, I'll be doing an in-depth review on Sublime Text 3 and possibly a few tutorials on it, so stay tuned.</p>
+
+
+
+</section>
+
+<?php include("../inc/blog-footer.php"); ?>
