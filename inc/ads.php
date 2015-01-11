f9c709a1a4d6dc268101e865af5045ebf0ca6619
Sam Webb <samwebb@codingtestsite.com>
2015-01-12 00:13:33 +0930

    created ads include page

---
+++ inc/ads.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- CTS Blog Pages Leaderboard Ad -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-5302100296062396"
+     data-ad-slot="6352603062"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
