94f05191f2c3e9662280d46bbb0b02b509df9ee7
salexzee <samwebb@codingtestsite.com>
2015-01-13 19:54:29 +0930

    added tweet button

--- blogs/custom-css-buttons.php
+++ blogs/custom-css-buttons.php
@@ -3,7 +3,7 @@
 
 <section class="container col-xs-10 blog-posts lead">
 
-	<h1>Custom CSS Buttons</h1>
+	<h1>Custom CSS Buttons <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://codingtestsite.com/blogs/custom-css-buttons.php" data-text="For those of you that are tired of regular browser styled #html buttons, you're in luck." data-via="SamTheWebb" data-hashtags="blog" data-dnt="true">Tweet</a></h1>
 
 	<?php include("../inc/ads.php"); ?>
 
@@ -76,5 +76,7 @@
     <?php include_once("../inc/comments.php"); ?>
     
 </section>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
 <?php include("../inc/blog-footer.php"); ?>
