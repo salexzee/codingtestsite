a8ab7eb4a4eb20c5223a660ff69333d98f835ead
salexzee <samwebb@codingtestsite.com>
2015-01-13 11:43:09 +0930

    minor fixes and added tweet button

--- blogs/code-snippets.php
+++ blogs/code-snippets.php
@@ -2,13 +2,13 @@
 
 <section class="container col-xs-10 blog-posts lead">
 
-	<h1>Why Save Code Snippets?</h1>
+	<h1>Why Save Code Snippets? <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://codingtestsite.com/blogs/code-snippets.php" data-text="Make #coding easier by saving snippets of code that you use on a regular basis." data-via="SamTheWebb" data-hashtags="blog" data-dnt="true">Tweet</a></h1>
 
     <?php include("../inc/ads.php"); ?>
 
-	<p>How many of you out there still manually type in your starting HTML? I'll admit it, I still do sometimes. I just haven't been able to completely break the habbit yet. A lot of the projects a beginner will be working on will probably be fairly small, so its not such a big deal, but what about when you want you're about to start a large project and you want to save all the time you can. That's when code snippets come in handy.</p>
+	<p>How many of you out there still manually type in your starting HTML? I'll admit it, I still do sometimes. I just haven't been able to completely break the habbit yet. A lot of the projects a beginner will be working on will probably be fairly small, so its not such a big deal, but what about when you're about to start a large project and you want to save all the time you can. That's when code snippets come in handy.</p>
 
-	<p>What are code snippets you may ask? Well I'll tell you. In simplest terms, they're pieces of code that you have saved somewhere so that you can use them in your projects. You don't need to make a code snippet for everything you do while coding, but what you want to create them for are the pieces of code you use most often. Take this for instance:</p>
+	<p>What are code snippets you may ask? Well I'll tell you. In simplest terms, they're pieces of code that you have saved somewhere so that you can use them in later projects. You don't need to make a code snippet for everything you do while coding, but what you want to create them for are the pieces of code you use most often. Take this for instance:</p>
 
 	<pre>
 &lt;!DOCTYPE html&gt;
@@ -33,14 +33,17 @@
 &lt;/html&gt;
 	</pre>
 
-	<p>This is the kind of thing I use in almost every project I work on. It might not be much, but when trying to save time, this is something you could save and use later so that you don't have to type it in every time. Over time when you're working on a large project, if you have a bunch of little or big snippets that you can insert, you'll end up with more time to work on any issues and bugs in your code.</p>
+	<p>This is the kind of thing I use in almost every HTML project I work on. It might not be much, but when trying to save time, this is something you could save and use later so that you don't have to type it in every time. Over time when you're working on a large project, if you have a bunch of little or big snippets that you can insert, you'll end up with more time to work on any issues and bugs in your code.</p>
 
-	<p>Depending on which text editor you use, you may be able to save your own code snippets so that you can call them with keywords. Sublime Text is one example of a text editor with that capability. Also, with ST, you can download code snippet packages people have made for your favorite languages and use those to save time. In the future, I'll be doing an in-depth review on Sublime Text 3 and possibly a few tutorials on it, so stay tuned.</p>
+	<p>Depending on which text editor you use, you may be able to save your own code snippets so that you can call them with keywords. Sublime Text 3 is one example of a text editor with that capability. Also, with ST3, you can download code snippet packages people have made for your favorite languages and use those to save time. In the future, I'll be doing an in-depth review on Sublime Text 3 and possibly a few tutorials on it, so stay tuned.</p>
 
 	<p>Hopefully this has shown you why you should start to use code snippets. If you start now, in the long term you'll start to see the benifits of this.</p>
     
     <?php include_once("../inc/comments.php"); ?>
     
 </section>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
 
 <?php include("../inc/blog-footer.php"); ?>
