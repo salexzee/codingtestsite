850d24f86171a7939210e279d09eb1bbcff386a1
salexzee <samwebb@codingtestsite.com>
2015-01-27 02:07:14 +0930

    updated home page

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<div class="container">
 			<div class="starter-template">
 				<img class="mainLogo col-xs-5 col-sm-4 img-rounded" src="images/ctsLogo.png">
-				<p class="lead col-xs-12 col-sm-8">Hey, my name is Sam Webb and welcome to CodingTestSite. This is the place to be for up-to-date insight, tutorials and reviews on all things web design and web development. In the past few months I've done a lot of learning about quite a few different technologies and now I'd like to give back some of the information I've attained. Hope you learn a thing or two.</p>
+				<p class="lead col-xs-12 col-sm-8">Hey, my name is Sam Webb and welcome to CodingTestSite. This is the place to be for up-to-date insight, tutorials and reviews on all things web design and web development. In the past year+ I've done a lot of learning about quite a few different technologies and now I'd like to give back some of the information I've attained. Hope you learn a thing or two.</p>
 
 			</div>
 		</div> 
