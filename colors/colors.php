e560f7f52b1918136f85d84ccc3835e131a8976d
salexzee <samwebb@codingtestsite.com>
2015-01-27 12:10:42 +0930

    commiting to make branch

--- colors/colors.php
+++ colors/colors.php
@@ -4,6 +4,13 @@ $colors = "on";
 include(ROOT_PATH . "inc/header.php");
 ?>
 
-Hello World!
+<main class="colors">
+	<section>
+		<h1>Welcome to My Color Picker</h1>
+	</section>
+	<section id="color-picker">
+		
+	</section>
+</main>
 
 <?php include(ROOT_PATH . "inc/footer.php"); ?>
--- js/app.js
+++ js/app.js
@@ -1,50 +1,63 @@
-/*                 */
-/* FORM VALIDATION */
-/*                 */
+$(document).ready(function(){
 
-$("#contactForm").validate({
-	rules: {
-		inputName: {
-			required: true,
-			minlength: 2
+	/*                 */
+	/* FORM VALIDATION */
+	/*                 */
+
+	$("#contactForm").validate({
+		rules: {
+			inputName: {
+				required: true,
+				minlength: 2
+			},
+			inputEmail: {
+				required: true,
+				email: true
+			},
+			inputMessage: {
+				required: true,
+				minlength: 50
+			}
 		},
-		inputEmail: {
-			required: true,
-			email: true
-		},
-		inputMessage: {
-			required: true,
-			minlength: 50
+		messages: {
+			inputName: {
+				required: "Please enter your name",
+				minlength: "Your name seems a bit short doesn't it?"
+			},
+			inputEmail: {
+				required: "Please enter your email address",
+				email: "Please enter a valid email address"
+			},
+			inputMessage: {
+				required: "Please enter your message",
+				minlength: "Your message seems a bit short doesn't it?"
+			}
 		}
-	},
-	messages: {
-		inputName: {
-			required: "Please enter your name",
-			minlength: "Your name seems a bit short doesn't it?"
-		},
-		inputEmail: {
-			required: "Please enter your email address",
-			email: "Please enter a valid email address"
-		},
-		inputMessage: {
-			required: "Please enter your message",
-			minlength: "Your message seems a bit short doesn't it?"
-		}
-	}
-});
+	});
 
 
 
-/*                        */
-/* HIDE AND SHOW TUTORIAL */
-/*                        */
-	
-$('.tut-show-hide-button-1').click(function(){
-	$('.tut-show-hide-inline-el').slideToggle();
-});
-
-$('.tut-show-hide-button-2').click(function(){
-	$('.tut-show-hide-inline-el-2').slideToggle();
-});
-
+	/*                        */
+	/* HIDE AND SHOW TUTORIAL */
+	/*                        */
 		
+	$('.tut-show-hide-button-1').click(function(){
+		$('.tut-show-hide-inline-el').slideToggle();
+	});
+
+	$('.tut-show-hide-button-2').click(function(){
+		$('.tut-show-hide-inline-el-2').slideToggle();
+	});
+
+
+	/*							*/
+	/* COLOR PICKER APPLICATION */
+	/*							*/
+
+
+
+
+
+
+
+});
