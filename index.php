0d12c217568762bd18e8101d0b14612b5e48f211
salexzee <samwebb@codingtestsite.com>
2015-01-28 01:47:46 +0930

    small updates

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		</div>
 		<div class="container">
 			<div class="starter-template">
-				<img class="mainLogo col-xs-5 col-sm-4 img-rounded" src="images/ctsLogo.png">
+				<img class="mainLogo col-xs-5 col-sm-4 img-rounded" src="<?php echo BASE_URL; ?>images/ctsLogo.png">
 				<p class="lead col-xs-12 col-sm-8">Hey, my name is Sam Webb and welcome to CodingTestSite. This is the place to be for up-to-date insight, tutorials and reviews on all things web design and web development. In the past year+ I've done a lot of learning about quite a few different technologies and now I'd like to give back some of the information I've attained. Hope you learn a thing or two.</p>
 
 			</div>
