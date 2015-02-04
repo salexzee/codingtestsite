8b6c51f3842d304d33cb47d629d9a6b5ac1a5acc
salexzee <samwebb@codingtestsite.com>
2015-02-05 00:25:35 +0930

    reverted to fix issues

--- inc/blog-header.php
+++ inc/blog-header.php
@@ -23,10 +23,10 @@
 			</div> 
 			<nav class="collapse navbar-collapse navHeaderCollapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li> <a href="<?php echo BASE_URL; ?>letters/">Letters</a></li>
-					<li> <a href="<?php echo BASE_URL; ?>colors/">Colors</a></li>
-					<li> <a href="<?php echo BASE_URL; ?>blogs/">Blog</a> </li>
-					<li> <a href="<?php echo BASE_URL; ?>contact/">Contact</a> </li>
+					<li> <a href="../letters/">Letters</a></li>
+					<li> <a href="../colors/">Colors</a></li>
+					<li> <a href="../blogs/">Blog</a> </li>
+					<li> <a href="../contact/">Contact</a> </li>
 				</ul>
 			</nav>
 		</div>
